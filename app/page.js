cacb3e4fb3882dd8c9dade8e5a0585789e3ed860
Chukwuemeka Uche <chucksilver68@gmail.com>
2025-08-18 16:49:53 +0100

    update on first push
    update on first push

---
+++ app/page.js
@@ -0,0 +1,12 @@
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="">NEW PROJECT</h1>
+    <Button>Subscribe</Button>
+    </div>
+  );
+}
